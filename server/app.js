affad2c16422793dba5a9994225ddd9e2b0ad99e
rudy wahjudi <rudy.wahjudi@gmail.com>
2017-06-22 00:14:00 +0700

    added eslint

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var cors = require('cors')
-var index = require('./routes/index')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use('/', index)
+app.use('/', index);
 
 const port = 3000;
-app.listen( port, () => {
+app.listen(port, () => {
   console.log(`bakwan is munchin to port ${port}`);
-})
+});
